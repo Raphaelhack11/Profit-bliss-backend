d665fe01a7a8c72d9e3e162625c79bdd86a5c081
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-27 15:08:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/auth.js";
-import depositRoutes from "./routes/deposit.js";
-import withdrawRoutes from "./routes/withdraw.js";
-import historyRoutes from "./routes/history.js";
+import transactionRoutes from "./routes/transactions.js";
 import adminRoutes from "./routes/admin.js";
 
 dotenv.config();
-
 const app = express();
-app.use(cors());
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/deposit", depositRoutes);
-app.use("/api/withdraw", withdrawRoutes);
-app.use("/api/history", historyRoutes);
+app.use("/api/transactions", transactionRoutes);
 app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 5000;
